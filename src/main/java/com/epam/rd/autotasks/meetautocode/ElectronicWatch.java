35f0b9ca80626e4fe9de9d13777d07d7938f9c3a
Daniil Laureanti <daniil.laureanti@gmail.com>
2021-11-24 14:30:47 +0200

    Completed task

--- src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -1,12 +1,23 @@
 package com.epam.rd.autotasks.meetautocode;
 
+import java.sql.Time;
 import java.util.Scanner;
 
 public class ElectronicWatch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int seconds = scanner.nextInt();
+        int seconds = Math.abs(scanner.nextInt());
+        currentDailyTime(seconds);
+    }
 
+    public static void currentDailyTime(int sec) {
+        Time time = new Time(00, 00, sec);
+        String printTime = time.toString();
 
+        if (printTime.charAt(0) == '0') {
+            System.out.println(printTime.substring(1));
+        } else {
+            System.out.println(printTime);
+        }
     }
 }
